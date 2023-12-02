c7693b2bd8ff41d768482773fea26d987323705a
anshuroy204 <123299175+anshuroy204@users.noreply.github.com>
2023-12-02 17:21:54 +0530

    Day 1

---
+++ Day1.py
@@ -0,0 +1,37 @@
+# print function
+# Day 1 - Python Print Function
+print("Day 1 - Python Print Function")
+
+# The function is declared like this:
+print("The function is declared like this:")
+
+# print('what to print')
+print("print('what to print')")
+
+# print("what to print")
+print("print(\"what to print\")")
+
+# String concatenation example
+# String concatenation is done with the "+" sign.
+print('String concatenation is done with the "+" sign.')
+
+# input() will take input from user in console and then print() will print Hello <input string>
+print("Hello " + input("What is your name"))
+
+
+# String length calculation
+# Calculate length of input string
+print(len(input()))
+
+# variable
+name = input("What is your name ?")
+print(name)
+
+# switching variable value
+a = input()
+b = input()
+c = a
+a = b
+b = c
+print("a: " + a)
+print("b: " + b)
